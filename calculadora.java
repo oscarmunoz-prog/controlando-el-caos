db59e8fdc1db17c2ebbaa712060008883d32a243
oscarmunoz-prog <oscarmunyozlopez@alumnes.ilerna.com>
2025-11-02 20:37:19 +0100

    Update calculadora.java

--- calculadora.java
+++ calculadora.java
@@ -10,27 +10,6 @@ public class calculadora {
 	        System.out.println("Multiplicación: " + (a * b));
 	        System.out.println("División: " + (a / b));
 
-        System . out . println ( "Suma: " + ( a + b ));
-        System . out . println ( "Resta: " + ( a - b ));
-        System . out . println ( "Multiplicación: " + ( a * b ));
-        System . out . println ( "División: " + ( a / b ));
-
-        System . out . println ( "Potencia " + a + "^" + b + ": " + potencia ( a , b )); 
-         System.out.println ( "Cuadrado de " + a + ": " + cuadrado(a));
-        System.out.println ( " Cubo de " + b + " : " + cubo ( b ) ) ; 
-    }
-
-     
-      public static double  potencia  ( int base  , int exponente  ) {
-        return  Math.pow ( base , exponente ) ;
-    }
-
-    public  static  int  cuadrado ( int  numero ) { 
-         return  numero * numero ;
-    }
-
-    public  static  int  cubo ( int  numero ) {
-        return  numero * numero * numero ;
 	    }
 	}
 
